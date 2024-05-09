e02414158c57491609bb573c212abad2473ff054
jongwoo97 <jongwoo@vinnsmail.com>
2024-05-09 16:33:18 +0900

    인명부 => 공지사항 데이터 및 처리 변경

--- app.js
+++ app.js
@@ -12,13 +12,32 @@ app.get('/tabulatorTest', function (req, res, next) {
   const tableData = require('./tableData.js');
   let resultData = [...tableData];
 
-  // searchText, 해당 searchText를 포함하는 name을 가진 아이템 filter
+  // 검색 구분searchType, searchText, searchType에서 searchText를 포함하는 아이템 filter
+  const searchType = req.query.searchText || '';
   const searchText = req.query.searchText || '';
-  if (searchText) resultData = resultData.filter((item) => item.name.includes(searchText));
+  if (searchText) {
+    if (searchType == 1) resultData = resultData.filter((item) => item.title.includes(searchText) || item.author.includes(searchText));
+    else if (searchType == 2) item.title.includes(searchText);
+    else if (searchType == 3) item.author.includes(searchText);
+  }
 
-  // department, 해당 department 아이템 filter
-  const department = req.query.department || '';
-  if (department) resultData = resultData.filter((item) => item.department === department);
+  // 기준일(baseDt)
+  const startBaseDt = req.query.startBaseDt || '';
+  const endBaseDt = req.query.endBaseDt || '';
+  if (startBaseDt && endBaseDt)
+    resultData = resultData.filter((item) => parseInt(item.baseDt) >= parseInt(startBaseDt) && parseInt(item.baseDt) <= parseInt(endBaseDt));
+
+  // 구분 (type)
+  const type = req.query.type || 1;
+  if (type == 2) resultData = resultData.filter((item) => item.type === '공지');
+  else if (type == 3) resultData = resultData.filter((item) => item.type === '언론보도');
+
+  // 상태 (status)
+  const status = req.query.status || 1;
+  if (status == 2) resultData = resultData.filter((item) => item.status === '등록');
+  else if (status == 3) resultData = resultData.filter((item) => item.status === '게시');
+  else if (status == 4) resultData = resultData.filter((item) => item.status === '예약');
+  else if (status == 5) resultData = resultData.filter((item) => item.status === '보류');
 
   // page
   const page = parseInt(req.query.page) || 0;
--- tableData.js
+++ tableData.js
@@ -1,7 +1,15 @@
 let tableData = [];
 
-for (let i = 1; i <= 100; i++) {
-  tableData.push({ name: `홍길동${i}`, tel: '010-1234-1234', department: i % 2 === 0 ? '개발팀' : '사업팀' });
+for (let i = 1; i <= 498; i++) {
+  tableData.push({
+    id: i,
+    type: i % 2 === 0 ? '공지' : '언론보도',
+    title: `테스트 제목${i % 100}`,
+    author: i % 7 === 0 ? 'IT관리자' : '슈퍼관리자',
+    baseDt: `20${String((i % 24) + 1).padStart(2, '0')}-${String((i % 12) + 1).padStart(2, '0')}-${String(i % 31).padStart(2, '0')}`,
+    registerDt: `20${String((i % 24) + 1).padStart(2, '0')}-${String((i % 12) + 1).padStart(2, '0')}-${String(i % 31).padStart(2, '0')}`,
+    status: i % 5 === 0 ? '등록' : i % 4 === 0 ? '게시' : i % 3 === 0 ? '예약' : '게시',
+  });
 }
 
 module.exports = tableData;
